2ede9e87d12c1eeefe4f37386b225a214dc6c367
kayesAshfaq <asfak7074@gmail.com>
2024-12-01 17:30:22 +0600

    chore: update Flutter environment path and add new dependencies in pubspec.yaml

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tn99249/Tools/flutter_3_24_3"
+export "FLUTTER_ROOT=/Users/tn99249/fvm/versions/3.24.3"
 export "FLUTTER_APPLICATION_PATH=/Users/tn99249/Developer/FlutterCodes/bmi-calculator-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
